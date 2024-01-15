31d3b96d81c64fe44053ba99cc9cdcb177699a60
kirtijaiswa1 <kirtihjaiswal@gmail.com>
2024-01-16 02:17:39 +0530

    changed few dependencies

--- src/components/incomeExpenses.js
+++ src/components/incomeExpenses.js
@@ -2,7 +2,7 @@ import React, {useContext} from 'react'
 import {GlobalContext} from '../context/GlobalState';
 
 export const IncomeExpenses = () => {
-  const {tansactions} = useContext(GlobalContext);
+  const {transactions} = useContext(GlobalContext);
   
   const amounts = transactions.map(transaction => transaction.amount);
   const income = amounts
